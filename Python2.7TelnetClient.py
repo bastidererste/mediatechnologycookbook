c3e96562fca3ee226aa6c56dbed971e17983e8b7
Sebastian Spiegl <s.spiegl@me.com>
2019-03-30 18:35:34 +0100

    Update Python2.7TelnetClient.py

--- Python2.7TelnetClient.py
+++ Python2.7TelnetClient.py
@@ -4,15 +4,21 @@ import socket
 IP = "127.0.0.1"
 PORT = 20000
 
-#MESSAGE = "HELLO"
-#MESSAGE = "HELLO\r\n"
-MESSAGE = '0200000000020d0a'.decode('hex')
-
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.settimeout(1000)
-
 s.connect((IP,PORT))
 
+# send string without carriage return and/or newline
+MESSAGE = "HELLO"
 s.send(MESSAGE)
 
+# send string without carriage return "\r" and "\n" newline
+MESSAGE = "HELLO\r\n"
+s.send(MESSAGE)
+
+# send bytes from hex-string
+MESSAGE = '0200000000020d0a'.decode('hex')
+s.send(MESSAGE)
+
+# close connection
 s.close()
