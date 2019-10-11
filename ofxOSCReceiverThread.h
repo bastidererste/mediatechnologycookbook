1c9118ccb0d439948ae254c33d3a1c1a7002658f
Sebastian Spiegl <s.spiegl@me.com>
2019-10-11 13:07:07 +0200

    Create ofxOSCReceiverThread.h

---
+++ ofxOSCReceiverThread.h
@@ -0,0 +1,92 @@
+
+/*
+
+
+        Add this header file (ofxOSCReceiverThread.h) to src folder
+        
+        in ofApp.h add...
+        
+        #include "ofxOSCReceiverThread.h"
+
+        public:
+          void receivedMessage(ofxOscMessage &m);
+          ofxOSCReceiverThread ofR;
+          
+        
+        in odApp.cpp add...
+        
+        void ofApp::setup(){
+    
+            ofR.setup(9999);
+            ofR.start();
+            ofAddListener(ofR.messageReceived, this, &ofApp::receivedMessage);
+        }
+        
+        void ofApp::receivedMessage(ofxOscMessage &m){
+        
+            ofLog(OF_LOG_NOTICE, "messagereceivedInEventNotify ");
+        }
+*/
+
+
+#include "ofxOsc.h"
+
+class ofxOSCReceiverThread : public ofThread
+{
+public:
+    
+    ~ofxOSCReceiverThread()
+    {
+        if (isThreadRunning()) stopThread();
+    }
+    
+    void setup(int port)
+    {
+        receiver.setup(port);
+    }
+    
+    void start()
+    {
+        startThread();
+    }
+    
+    void stop()
+    {
+        stopThread();
+    }
+    
+    void threadedFunction()
+    {
+        ofLog(OF_LOG_NOTICE, "thread started ");
+        
+        while (isThreadRunning())
+        {
+            if (lock())
+            {
+                while (receiver.hasWaitingMessages())
+                {
+                    
+                    if (receiver.getNextMessage(msg))
+                    {
+                        ofxOscMessage m = msg;
+                        ofNotifyEvent(messageReceived, m, this);
+                        ofLog(OF_LOG_NOTICE, "messagereceived ");
+                        
+                    }
+                    
+                }
+            }
+            unlock();
+        }
+    }
+    
+    
+    
+    ofEvent<ofxOscMessage> messageReceived;
+    
+private:
+    
+    ofxOscReceiver receiver;
+    ofxOscMessage msg;
+    
+};
