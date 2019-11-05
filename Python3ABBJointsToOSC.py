ed6f541767e8809c4f09a23c194d084e1d1980cd
Sebastian Spiegl <s.spiegl@me.com>
2019-11-05 21:45:08 +0100

    Create Python3ABBJointsToOSC.py

---
+++ Python3ABBJointsToOSC.py
@@ -0,0 +1,22 @@
+
+import socket
+from pythonosc import udp_client
+import numpy as np
+
+
+HOST = '192.168.1.155' # IP of the robot
+PORT = 7001
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((HOST, PORT))
+
+osc_client = udp_client.SimpleUDPClient("192.168.188.20", 11111)
+while True:
+	data = sock.recv(1024)
+	stringData = data.decode('utf-8').replace('>','')
+	x = np.array(stringData.split())
+	print(x)
+	osc_client.send_message("/robo", x)
+
+
+sock.close()
