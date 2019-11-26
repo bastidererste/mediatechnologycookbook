f55e9ead0385f977512151ea7c5b3abc65ba1976
Sebastian Spiegl <s.spiegl@me.com>
2019-11-26 16:58:21 +0100

    Update C#LindyMatrixCGIPostRequest.cs

--- C#LindyMatrixCGIPostRequest.cs
+++ C#LindyMatrixCGIPostRequest.cs
@@ -12,30 +12,32 @@ namespace web
         public static void Main(string[] args)
         {
             System.Net.ServicePointManager.Expect100Continue = false;
+            //Expect100Continue issue: https://haacked.com/archive/2004/05/15/http-web-request-expect-100-continue.aspx/            
 
             WebRequest request = WebRequest.Create("http://192.168.1.44/m.cgi");
-
             request.Method = "POST";
-            request.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
-            request.ContentLength = 17;
             
-            var postData = "param1=" + Uri.EscapeDataString("5"); //Mapping
-            postData += "&param2=" + Uri.EscapeDataString("1"); //MappingIndex
-            
-            
-            var data = Encoding.ASCII.GetBytes(postData);         
+            //Switch between saved mappings on Lindy 8x8 HDMI 2.0 18G Matrix Switch with post webrequest
+            var postData = "param1=" + Uri.EscapeDataString("5"); //Option - use 5 to use saved mappings
+            postData += "&param2=" + Uri.EscapeDataString("1"); //MappingIndex - index of mapping to select from list of saved mappings
+
+            var data = Encoding.ASCII.GetBytes(postData);
+
+
             using (var stream = request.GetRequestStream())
             {
                 stream.Write(data, 0, data.Length);
             }
 
+
+
+
             var response = (HttpWebResponse)request.GetResponse();
 
             var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
 
-            Console.WriteLine(responseString);
+            Console.WriteLine(responseString + data.Length);
 
         }
     }
 }
-
